154ae1a748f056ec0e81fd248c8cd2c5c3166ba2
Kory33 <konryoitdv@gmail.com>
2017-04-11 15:57:56 +0200

    apply license

--- src/main/java/com/github/kory33/mojangapi/MojangAPI.java
+++ src/main/java/com/github/kory33/mojangapi/MojangAPI.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ * 
+ * Copyright 2017 Kory3.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.github.kory33.mojangapi;
 
 import java.io.BufferedReader;
@@ -9,10 +32,11 @@ import java.util.concurrent.CompletableFuture;
 
 import org.json.JSONObject;
 
+
+
 /**
  * Wrapper util class for making requests to Mojang API
  * @author kory
- *
  */
 public class MojangAPI {
     public static String USERNAME_TO_UUID_ENDPOINT_URL = "https://api.mojang.com/users/profiles/minecraft/";
